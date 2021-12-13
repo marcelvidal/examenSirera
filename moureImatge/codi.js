649dce798b3bb2432b0ef0810cf16206f87c9318
marcelvidal <95225187+marcelvidal@users.noreply.github.com>
2021-12-13 23:50:05 +0100

    mou verticalment pero no horitzontal

--- moureImatge/codi.js
+++ moureImatge/codi.js
@@ -6,33 +6,114 @@ class moviment {
 		var heightScreen = window.innerHeight;
 		var widthScreen = window.innerWidth;
 
-
-
+		this.moviment = false;
+	
 
 		// CREAR TAG DIV I TAG IMG
 		this.div = document.createElement("div"); // CREAR DIV
 		document.body.appendChild(this.div); // AFEGIRLO COM A FILL DEL BODY
 		this.img = document.createElement("img"); // CREAR IMATGE 
-		
+
 
 
 		// MODIFICAR IMATGE
 
 		this.img.src = imatge;  // CREAR SRC IMATGE I AFEGIRLI DEL PARAMETRE IMATGE
-		this.img.style.width = 200+"px";
-		this.img.style.height = 200+"px";
+		this.img.style.width = 200 + "px";
+		this.img.style.height = 200 + "px";
+
+// POSAR POSICIONS APART PER PODERLES MANIPULAR DESPRES !!!!IMPORTA
+		this.vertical = getRandom(0, (heightScreen - 200)) ; // RANDOM POSICIO X/Y IMATGES 
+		this.horitzontal = getRandom(0, (widthScreen - 200)); 
 
 
-		this.img.style.top = getRandom(0,widthScreen)+"px"; // FALTA DIRLI LAMPLADA DE LA PANTALLA
-		this.img.style.left = getRandom(0,heightScreen)+"px"; // FALTA DIRLI
-		this.div.appendChild(this.img); // AFEGIRLO COM A FILL DEL DIV
 
-		this.velocitat = velocitat;  
+		this.img.style.top = this.vertical+ "px";
+        this.img.style.left = this.horitzontal+ "px";
+
+ 		// AFEGIRLO COM A FILL DEL DIV
+		this.div.appendChild(this.img);
+
+		this.velocitat = velocitat;
+
+	
+		setInterval(this.moureImatge, this.velocitat, this);
+
+
+
 
 
 	}
+
+
+	// METODES DEL OBJECTE
+
+	moureImatge(t_) {
+
+
+		var topImatge = t_.img.style.top;
+		var leftImatge = t_.img.style.left;
+		topImatge = Number.parseInt(topImatge);
+
+
+
+
+///PREEGUNTAR PERQUE EL OBJECTE NO ES MOU SI LA VARIABLE  MOVIMENT NO ESTA DECLARADA COM A OBJECTE
+
+
+		
+		if(topImatge+200 >= window.innerHeight){
+			t_.moviment =false;
+		}
+
+		
+		if (topImatge < 0) {
+			t_.moviment = true;
+		}
+		
+	
+		if (t_.moviment) {
+			t_.vertical++; // POSICIO DE LA IMATGE
+			t_.img.style.top = t_.vertical +"px";
+
+		}
+		if(!t_.moviment){
+			t_.vertical--;
+			t_.img.style.top = t_.vertical +"px";
+		}
+
+
+
+
+
+
+	}
+
+	set setImatge(img) {
+		this.img = img;
+	}
+
+	get getImatge() {
+		return this.img;
+	}
+
+	set setVelocitat(velocitat) {
+		this.velocitat = velocitat;
+	}
+
+	get getVelocitat() {
+		return this.velocitat;
+	}
+
+
+
 }
 
+
+
+/// FUNCIONS
+
+
 function getRandom(min, max) {
-    return Math.random() * (max - min) + min;
+	return Math.random() * (max - min) + min;
 }
